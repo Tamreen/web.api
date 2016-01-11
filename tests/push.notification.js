7d1b03c08c8a575de5b689ee29a64280df58fbdb
hossamzee <hossam_zee@yahoo.com>
2016-01-12 02:32:54 +0300

    Fix the readme file to display all API routes.

--- tests/push.notification.js
+++ tests/push.notification.js
@@ -1,40 +1,29 @@
 
-// http://stackoverflow.com/questions/8495687/split-array-into-chunks
-Array.prototype.chunk = function(chunkSize){
-
-    var chunksArray = [];
-
-    for (var i=0; i<this.length; i+=chunkSize){
-        chunksArray.push(this.slice(i, i+chunkSize));
-    }
-
-    return chunksArray;
-}
-
-Promise = require('bluebird');
-using = Promise.using;
-nconf = require('nconf');
-
-// Get the config information.
-nconf.argv().env().file({file: './configs/variables.json'});
-
-//
-require('../services/PushNotificationService');
-require('../services/DatabaseService');
+require('../server');
 
 var message = {
 	icon: '',
 	content: 'تحديث خُرافي متوفّر الآن في المتجر',
-	sound: 'ping.aiff',
+	sound: 'AngelChoir.aif',
 	trainingName: '',
 };
 
-// Get the users
-DatabaseService.query('select * from users where deviceToken is not null')
+var users = [
+	{deviceType: 'ios', deviceToken: ''},
+];
 
 //
-.then(function(users){
-	return PushNotificationService.pushMessageToUsers(message, users);
-});
+PushNotificationService.pushMessageToUsers(message, users);
+
+// // Get the users
+// DatabaseService.query('select * from users where deviceToken is not null')
+
+// //
+// .then(function(users){
+// 	//console.log(users);
+// 	return PushNotificationService.pushMessageToUsers(message, users);
+// });
+
+// 
 
 // console.log(message);
--- tests/temp.js
+++ tests/temp.js
@@ -1,2 +1,11 @@
 
 require('../server');
+
+// console.log(router.stack);
+Promise.each(router.stack, function(layer){
+
+	var route = layer.route; var path = route.path;
+	var method = Object.keys(route.methods)[0].toUpperCase();
+
+	console.log(method, path);
+});
